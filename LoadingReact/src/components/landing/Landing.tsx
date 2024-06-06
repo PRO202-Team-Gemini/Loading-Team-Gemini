4a20afdf7be0800709bb6c75ea3dd8cb33569c73
Solveig Årvik <soar004@student.kristiania.no>
2024-06-06 11:36:02 +0200

    Replaced btn with QR Code
    Created QR code with qrcode.react and linked to localhost signin

--- LoadingReact/src/components/landing/Landing.tsx
+++ LoadingReact/src/components/landing/Landing.tsx
@@ -2,11 +2,14 @@ import React from "react"; // Import React
 import { useNavigate } from "react-router-dom";
 import { LANDING_TEXT } from "../constants/landingpageText";
 import "../styles/landing.css";
+import QRCode from "qrcode.react";
 
 const { TITLE, SUBTITLE, PARAGRAPH1, BUTTON_TEXT } = LANDING_TEXT;
 
 const Landing = () => {
   const navigate = useNavigate();
+
+  const qrValue = "http://localhost:5173/signIn"; // Change this to the actual URL
 
   const handleClick = () => {
     navigate("/turnPhone");
@@ -20,14 +23,9 @@ const Landing = () => {
       </div>
       <div className="text-center card2 shadow rounded p-4 m-2 card-body">
         <p>{PARAGRAPH1}</p>
-        <button
-          type="button"
-          className="btn btn-success mt-3"
-          onClick={handleClick}
-          aria-label={BUTTON_TEXT}
-        >
-          {BUTTON_TEXT}
-        </button>
+        <div className="qr-code-container mt-3">
+          <QRCode value={qrValue} size={100} />
+        </div>
       </div>
     </article>
   );
