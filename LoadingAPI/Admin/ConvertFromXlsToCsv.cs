09e07164c763eb7b2f0aa677a01a96528eac9cc2
Vegard Falldalen <vegfall@gmail.com>
2024-06-11 15:41:36 +0200

    Hopefully fixed conversion of xlsx to csv

--- LoadingAPI/Admin/ConvertFromXlsToCsv.cs
+++ LoadingAPI/Admin/ConvertFromXlsToCsv.cs
@@ -1,4 +1,5 @@
-﻿using Spire.Xls;
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using System.Text;
 
 namespace LoadingAPI.Admin
@@ -10,11 +11,30 @@ namespace LoadingAPI.Admin
             string xlsLoadPath = Path.Combine(Environment.CurrentDirectory, @"Admin\Datafiles", xlsFileName);
             string csvSavePath = Path.Combine(Environment.CurrentDirectory, @"Admin\Datafiles", csvFileName);
 
-            Workbook workbook = new ();
-            workbook.LoadFromFile(xlsLoadPath);
+            IWorkbook workbook;
+            using (FileStream file = new FileStream(xlsLoadPath, FileMode.Open, FileAccess.Read))
+            {
+                workbook = new XSSFWorkbook(file);
+            }
 
-            Worksheet sheet = workbook.Worksheets[0];
-            sheet.SaveToFile(csvSavePath, ",", Encoding.UTF8);
+            ISheet sheet = workbook.GetSheetAt(0);
+            StringBuilder csvData = new();
+
+            for (int i = 0; i <= sheet.LastRowNum; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                for (int j = 0; j < row.LastCellNum; j++)
+                {
+                    csvData.Append(row.GetCell(j));
+                    if (j < row.LastCellNum - 1)
+                    {
+                        csvData.Append(",");
+                    }
+                }
+                csvData.Append("\n");
+            }
+
+            File.WriteAllText(csvSavePath, csvData.ToString(), Encoding.UTF8);
         }
     }
 }
